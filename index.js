00d531f7476702da6a206fc977cd7ba495575d10
Megharth <m.c.lakhataria@gmail.com>
2019-03-07 22:13:19 +0530

    changes

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-let image = document.getElementById("image")
-let height = image.naturalHeight
-let width = image.naturalWidth
+let name
+window.onload = function() {
+  let image = document.getElementById("image")
+  let height = image.naturalHeight
+  let width = image.naturalWidth
 
-let body = document.getElementsByTagName("body")[0]
-body.style.height = height + "px"
-body.style.width = width + "px"
+  let body = document.getElementsByTagName("body")[0]
+  body.style.height = height + "px"
+  body.style.width = width + "px"
 
-let certificateContainer = document.getElementById("certificate")
-certificateContainer.style.height = height + "px"
-certificateContainer.style.width = width + "px"
+  let certificateContainer = document.getElementById("certificate")
+  certificateContainer.style.height = height + "px"
+  certificateContainer.style.width = width + "px"
 
-let name = "Megharth Lakahataria"
-let nameContainer = document.getElementById("name")
-nameContainer.innerText = name
-nameContainer.style.top = (height / 2) + 200 +  "px"
-nameContainer.style.width = width + "px"
-
-
-let id = "1203921oxx"
-let idContainer = document.getElementById("id")
-idContainer.innerText = id
-idContainer.style.top = height/15 + "px"
-idContainer.style.width = width + "px"
+  name = "Megharth Lakahataria"
+  let nameContainer = document.getElementById("name")
+  nameContainer.innerText = name
+  nameContainer.style.top = (height / 2) + 200 +  "px"
+  nameContainer.style.width = width + "px"
 
 
-/*let waterMarkContainer = document.getElementById("watermark")
-console.log(waterMarkContainer)
-waterMarkContainer.innerText = name
-waterMarkContainer.style.top = ((height/2) - 100) + "px"
-waterMarkContainer.style.width = width + "px"*/
+  let id = "1203921oxx"
+  let idContainer = document.getElementById("id")
+  idContainer.innerText = id
+  idContainer.style.top = height/15 + "px"
+  idContainer.style.width = width + "px"
+
+
+  /*let waterMarkContainer = document.getElementById("watermark")
+  console.log(waterMarkContainer)
+  waterMarkContainer.innerText = name
+  waterMarkContainer.style.top = ((height/2) - 100) + "px"
+  waterMarkContainer.style.width = width + "px"*/
+
+  toPdf()
+}
 
 // PDF CONVERSION
 
@@ -44,4 +49,3 @@ let toPdf = async function () {
   });
 }
 
-toPdf()
